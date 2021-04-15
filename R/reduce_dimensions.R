d1ccfa8cd76387205f97031eee508c42ef9cd672
JonThom <jonatan.thompson@gmail.com>
2021-04-15 15:37:13 +0200

    set RunPCA verbose=F

---
+++ R/reduce_dimensions.R
@@ -0,0 +1,27 @@
+
+#' Estimate dataset dimensionality
+#'
+#' @param x : Seurat or SingleCellExperiment object
+#' @param max_dim : max number of dimensions to try
+#' @param seed.use : random seed
+#'
+#' @return integer number of dimensions, <= max_dim
+#' @export
+#'
+#' @examples estimate_dimensions(seurat_obj)
+dim_red_est <- function(x,
+                        max_dim=75,
+                        seed.use=12345) {
+
+  if(is(x, "SingleCellExperiment")) {
+    x <- as.Seurat(x)
+  }
+
+  x %>% RunPCA(., npcs=max_dim, seed.use=seed.use, verbose = F) -> x
+
+  dims <- round(as.numeric(intrinsicDimension::maxLikGlobalDimEst(data = x@reductions$pca[, 1:max_dim], k = 20)))
+
+  return(dims)
+}
+
+
